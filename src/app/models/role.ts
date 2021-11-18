11548ebe8a165f309d3b90a951a9f297ba2da5f1
unknown <Blake.Dobbins@alumni.ou.edu>
2021-11-18 16:52:24 -0600

    models

--- src/app/models/role.ts
+++ src/app/models/role.ts
@@ -1,2 +1,13 @@
 export class Role {
+
+    public roleId : number;
+    public role : string;
+  
+    constructor(roleId : number, role : string) {
+  
+      this.roleId = roleId;
+      this.role = role;
+  
+    }
+  
 }
--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,3 +1,22 @@
+import { Role } from "./role";
+
 export class User {
+
+    public userId : number;
+    public firstName : string;
+    public lastName : string;
+    public email : string;
+    public role : Role;
+  
+    constructor(userId : number, firstName : string, lastName : string, email : string, role : Role) {
+  
+        this. userId = userId;
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.email = email;
+      this.role = role;
+  
+    }
+  
 
 }
