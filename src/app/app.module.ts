dc442ab83c3b1f7579e34930dc858d0ac9618ba1
unknown <Blake.Dobbins@alumni.ou.edu>
2021-11-18 16:21:37 -0600

    building profile view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent
+  
   ],
   imports: [
     BrowserModule,
