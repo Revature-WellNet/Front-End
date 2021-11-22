7758cfa84e752ca65c46f8e89de4472967cbb079
changj34 <changj34@uw.edu>
2021-11-22 13:53:00 -0800

    modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { FirebaseService } from './user-auth/services/firebase.service';
 import {AngularFireModule} from '@angular/fire/compat';
 import { environment } from 'src/environments/environment';
 import { UserAuthModule } from './user-auth/user-auth.module';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { ProfileComponent } from './components/profile/profile.component';
 import { RegistrationComponent } from './components/registration/registration.component';
