4060ca871a2528d4a2ebeff8d00df095137ac744
unknown <Blake.Dobbins@alumni.ou.edu>
2021-11-22 16:19:53 -0600

    fixed for Sam

--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -210,6 +210,7 @@ export class RegistrationComponent implements OnInit {
         // console.log(data[0].role.role);
 
         this.registrationSender.routeToNurseComponent(Object(data).role.role);
+        console.log(Object(data).role.role);
 
       });
 
