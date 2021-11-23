d43a318eee01ff81c9e1222c870ed5e371229c5b
Steven Hanley <Steven.Hanley@revature.net>
2021-11-23 09:23:37 -0600

    Many symptoms starting

--- src/app/components/diagnosis/diagnosis.component.ts
+++ src/app/components/diagnosis/diagnosis.component.ts
@@ -10,6 +10,8 @@ export class DiagnosisComponent implements OnInit {
 
   diagnosis: string = ' ';
   symptoms: string = ' ';
+  symptom: string = ' ';
+  iter: number = 0;
   constructor(private patientService: PatientService) { 
 
   }
@@ -17,18 +19,47 @@ export class DiagnosisComponent implements OnInit {
   ngOnInit() {
   }
 
-  onSubmit(diagnosis: string, symptoms: string){
-    console.log(symptoms, ' ', diagnosis);
+  onSubmit(diagnosis: string){
+    console.log(this.symptoms, ' ', diagnosis);
 
   }
 
+  addSymptom(symptom: string) {
+    
+    this.symptoms.concat(symptom[this.iter]+"; ");
+    this.iter++;
+    console.log(this.iter);
+    let row = document.createElement('div');
+    row.className = 'row';
+    row.innerHTML = 
+    '<input type= "text" '+ 
+    'class= "form-control col-sm-3" '+ 
+    'name= "symptom'+this.iter+'" '+
+    'id= ""symptom'+this.iter+'" '+
+    '[(ngModel)] = "symptom'+this.iter+'"/> '+
+    '<button type="button" class="btn btn-secondary col-sm-1" (click)="addSymptom(symptom'+this.iter+')">+</button> '+
+    '<br> ';
+  document.querySelector('.symptomInput')?.appendChild(row);
+  console.log(row);
+}
+  //   row.innerHTML = 
+  //     '<input type= "text" '+ 
+  //     'class= "form-control col-sm-3"'+ 
+  //     'name= "symptom'+this.iter+'"'+
+  //     'id= ""symptom'+this.iter+'"'+
+  //     '[(ngModel)] = ""symptom'+this.iter+'""/>'+
+  //     '<button type="button" class="btn btn-secondary col-sm-1" (click)="addSymptom(symptom'+this.iter+')">+</button>'+
+  //     '<br>';
+  //   document.querySelector('.symptomInput')?.appendChild(row);
+  // }
+  /*
   rejectDiagnosis(){
     // logic to inform nurse to revisit with patient.
   }
 
   overrideDiagnosis(diagnosis: string){
     // perhaps format invalid diagnosis (make italic or someting) then append new diagnosis.
-  }
+  }*/
 
   prescribeTreatment(treatment: string){}
 
