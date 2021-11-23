edd8a491ac1c76a4012eab7d564e3f8eeec2c986
vgcaccamo <vgcaccamo@gmail.com>
2021-11-23 12:29:21 -0500

    refactored symptoms field

--- src/app/components/diagnosis/diagnosis.component.ts
+++ src/app/components/diagnosis/diagnosis.component.ts
@@ -10,41 +10,54 @@ export class DiagnosisComponent implements OnInit {
 
   diagnosis: string = ' ';
   symptoms: string = ' ';
-  symptom: string = ' ';
   iter: number = 0;
-  constructor(private patientService: PatientService) { 
+  constructor(private patientService: PatientService) {
 
   }
 
   ngOnInit() {
   }
 
-  onSubmit(diagnosis: string){
-    console.log(this.symptoms, ' ', diagnosis);
+  onSubmit(symptoms: string, diagnosis: string){
+    console.log(symptoms, diagnosis);
 
   }
 
-  addSymptom(symptom: string) {
-    
-    this.symptoms.concat(symptom[this.iter]+"; ");
-    this.iter++;
-    console.log(this.iter);
-    let row = document.createElement('div');
-    row.className = 'row';
-    row.innerHTML = 
-    '<input type= "text" '+ 
-    'class= "form-control col-sm-3" '+ 
-    'name= "symptom'+this.iter+'" '+
-    'id= ""symptom'+this.iter+'" '+
-    '[(ngModel)] = "symptom'+this.iter+'"/> '+
-    '<button type="button" class="btn btn-secondary col-sm-1" (click)="addSymptom(symptom'+this.iter+')">+</button> '+
-    '<br> ';
-  document.querySelector('.symptomInput')?.appendChild(row);
-  console.log(row);
+  addSymptom() {
+    // attempts at adding rows commented out
+    // this.iter++;
+    // let row = document.createElement('div');
+    // row.className = 'row';
+    // row.innerHTML =
+    // '<input type= "text" '+
+    // 'class= "form-control col-sm-3" '+
+    // 'name= "symptom-'+this.iter+'" '+
+    // 'id= ""symptom-'+this.iter+'" '+
+    // '[(ngModel)] = "symptom['+this.iter+']"/>'
+    // document.querySelector('.symptomInput')!.appendChild(row);
+    // console.log(row);
+
+        // attempts at adding rows commented out
+  //   this.symptoms.concat(symptom[this.iter]+"; ");
+  //   this.iter++;
+  //   console.log(this.iter);
+  //   let row = document.createElement('div');
+  //   row.className = 'row';
+  //   row.innerHTML =
+  //   '<input type= "text" '+
+  //   'class= "form-control col-sm-3" '+
+  //   'name= "symptom'+this.iter+'" '+
+  //   'id= ""symptom'+this.iter+'" '+
+  //   '[(ngModel)] = "symptom'+this.iter+'"/> '+
+  //   '<button type="button" class="btn btn-secondary col-sm-1" (click)="addSymptom(symptom'+this.iter+')">+</button> '+
+  //   '<br> ';
+  // document.querySelector('.symptomInput')?.appendChild(row);
+  // console.log(row);
 }
-  //   row.innerHTML = 
-  //     '<input type= "text" '+ 
-  //     'class= "form-control col-sm-3"'+ 
+    // attempts at adding rows commented out
+  //   row.innerHTML =
+  //     '<input type= "text" '+
+  //     'class= "form-control col-sm-3"'+
   //     'name= "symptom'+this.iter+'"'+
   //     'id= ""symptom'+this.iter+'"'+
   //     '[(ngModel)] = ""symptom'+this.iter+'""/>'+
