af8909f9c5cb29699af5ec4b8634a8809055a28f
tubigervie <etubig@uci.edu>
2021-11-24 14:14:21 -0800

    Nav bar logout hookup

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from '../user-auth/services/firebase.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private firebaseService : FirebaseService) { }
 
   ngOnInit(): void {
+  }
+
+  logout()
+  {
+    this.firebaseService.logout();
   }
 
 }
