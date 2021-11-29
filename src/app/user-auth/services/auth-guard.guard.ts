7dd8b7c3f676010833f732f2f914d28762c772fb
tubigervie <etubig@uci.edu>
2021-11-29 15:15:52 -0800

    auth guard covid-verification WIP

--- src/app/user-auth/services/auth-guard.guard.ts
+++ src/app/user-auth/services/auth-guard.guard.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map, take, tap } from 'rxjs/operators';
+import { Covid19VerificationService } from 'src/app/services/covid19-verification.service';
 import { FirebaseService } from './firebase.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuardGuard implements CanActivate {
-  constructor(private service:FirebaseService, private router:Router){}
+  constructor(private service:FirebaseService, private router:Router, private cvService : Covid19VerificationService){}
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
@@ -26,12 +27,29 @@ export class AuthGuardGuard implements CanActivate {
 
  
   const userData = JSON.parse(localStorage.getItem('userinfo') || '{}');
- console.log("user data: " + JSON.stringify(userData));
+  const covidData = JSON.parse(localStorage.getItem('covidInfo') || '{}');
+
   if((userData._token==null) ){
-  
-    return this.router.createUrlTree([''])
-   
+    console.log("no user data");
+    return this.router.createUrlTree(['']);
   }
+  // if(covidData.userId == null)
+  // {
+  //   console.log("in here>>>>???");
+  //   return this.router.createUrlTree(['']);
+  // }
+  // if(covidData.covid == true)
+  // {
+  //   return this.router.createUrlTree(['lockout']);
+  // }
+  // console.log("before date compare");
+  // let now = new Date().getTime();
+  // let date = new Date(covidData.timestamp).getTime();
+  // console.log("now - date: " + (now-date >= 86400000))
+  // if( now - date >= 86400000)
+  // {
+  //   return this.router.navigate(['covid-verification']);
+  // }  
  
   return true;
   }
