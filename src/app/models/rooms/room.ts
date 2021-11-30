2798595b20a564bba85d7623fd2364ca099f1db8
mcmarioman <marvidal@hotmail.com>
2021-11-30 07:22:44 -0800

    1130

--- src/app/models/rooms/room.ts
+++ src/app/models/rooms/room.ts
@@ -6,12 +6,14 @@ export class Room {
     roomStatus:number;
     area:Area;
     patients:string[];
+    highlighted:boolean;
 
-    constructor(roomId:number, roomNumber:number, area:Area, roomStatus:number, patients:string[]){
+    constructor(roomId:number, roomNumber:number, area:Area, roomStatus:number, patients:string[], highlighted:boolean){
         this.roomId = roomId;
         this.roomNumber = roomNumber;
         this.area = area;
         this.roomStatus = roomStatus;
         this.patients = patients;
+        this.highlighted = highlighted;
     }
 }
