805c38c85adfd195f9bf503e24d1755ef78e5d6f
AAcosta9600 <91551938+AAcosta9600@users.noreply.github.com>
2021-12-06 10:28:40 -0500

    spec

--- src/app/lockout/lockout.component.spec.ts
+++ src/app/lockout/lockout.component.spec.ts
@@ -1,14 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Covid19VerificationService } from '../services/covid19-verification.service';
+import { FirebaseService } from '../user-auth/services/firebase.service';
 
 import { LockoutComponent } from './lockout.component';
+
+import { of } from 'rxjs';
 
 describe('LockoutComponent', () => {
   let component: LockoutComponent;
   let fixture: ComponentFixture<LockoutComponent>;
 
+  let serviceStub: any;
+
   beforeEach(async () => {
+    serviceStub
+
     await TestBed.configureTestingModule({
-      declarations: [ LockoutComponent ]
+      declarations: [ LockoutComponent ], 
+      providers:[{provide: FirebaseService, useValue: serviceStub}, {provide: Covid19VerificationService, useValue: serviceStub}]
     })
     .compileComponents();
   });
@@ -23,3 +32,5 @@ describe('LockoutComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
+it('should call ')
