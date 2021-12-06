e6959f7973eb6cb7bf63fd2fe28a6e59b7a14817
umi729 <umer.zahid@revature.net>
2021-12-05 20:42:58 -0700

    testing

--- src/app/user-auth/services/firebase.service.spec.ts
+++ src/app/user-auth/services/firebase.service.spec.ts
@@ -1,13 +1,24 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { AngularFireModule } from '@angular/fire/compat';
+import { RouterTestingModule } from '@angular/router/testing';
+import { environment } from 'src/environments/environment';
 
 import { FirebaseService } from './firebase.service';
 
 describe('FirebaseService', () => {
   let service: FirebaseService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(FirebaseService);
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        AngularFireModule.initializeApp(environment.firebase),
+        HttpClientTestingModule,
+      ],
+      declarations: [],
+      providers: [],
+    }).compileComponents();
   });
 
   it('should be created', () => {
