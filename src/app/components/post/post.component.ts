405d7bd7ad85d0fb3673301a2c2eb2c865678af9
anjohnsonoh <anjohnsonoh@gmail.com>
2022-04-27 21:46:22 -0400

    adding forum

---
+++ src/app/components/post/post.component.ts
@@ -0,0 +1,160 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Post } from 'src/app/models/post';
+import { Comment } from 'src/app/models/comment';
+import { PostService } from 'src/app/services/post.service';
+import { CommentService } from 'src/app/services/comment.service';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
+import { Userpost } from 'src/app/models/userpost';
+import { Usercomment } from 'src/app/models/usercomment';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent implements OnInit {
+  user!: User;
+  @Input() post!: Userpost;
+  @Input() size!: string;
+  comments: Usercomment[] = [];
+  showComment: boolean = false;
+  @Input() commentBody!: string | null;
+  @Input() newComment: boolean = false;
+  editPost: boolean = false;
+  editComment: boolean = false;
+  newTitle!: string | null;
+  newDescription!: string | null;
+  id!: number;
+  newBody!: string | null;
+
+  constructor(
+    private postService : PostService,
+    private commentService : CommentService,
+    private userService : UserService,
+    private route : Router
+  ) { }
+
+  ngOnInit(): void {
+    this.getComments();
+
+    const userData = JSON.parse(localStorage.getItem('userinfo') || '{}');
+    this.userService.getUser(userData.id).subscribe(
+      (response : User) => {
+        this.user = response;
+      }
+    );
+  }
+
+  getComments() {
+    this.comments = [];
+    this.postService.findCommentsByPost(this.post.pId).subscribe(
+      (c : Comment[]) => {
+        c.forEach(
+          (i: Comment) => {
+            this.userService.getUser(i.authorId).subscribe(
+              (u: User) => {
+                var com: Usercomment = {
+                  cId: i.cId,
+                  body: i.body,
+                  authorId: i.authorId,
+                  author: u,
+                  created: i.created,
+                  root: i.root
+                }
+                this.comments.push(com);
+              }
+            );
+          }
+        );
+      }
+    );
+  }
+
+  editMyPost() {
+    this.newTitle = this.post.title;
+    this.newDescription = this.post.description;
+    this.editPost = true;
+    this.size = '47vh';
+  }
+
+  updatePost() {
+    this.post.title = this.newTitle;
+    this.post.description = this.newDescription;
+    this.postService.updatePost(this.post.pId, this.post).subscribe(
+      () => {
+        this.postService.findPostById(this.post.pId);
+      }
+    );
+    this.editPost = false;
+    this.size = '52vh';
+  }
+
+  deletePost() {
+    for(var element of this.comments) {
+      var id = element.cId;
+      this.commentService.deleteComment(id);
+    }
+    this.postService.deletePost(this.post.pId);
+    this.route.navigate(['/']).then( () => this.route.navigate(['/forum']));
+  }
+
+  deleteComment(id: number) {
+    this.commentService.deleteComment(id).subscribe(
+      () => {
+        this.getComments();
+      }
+    );
+  }
+
+  editMyComment(id: number, text: string) {
+    this.showComment = false;
+    this.size = '52vh';
+    this.editComment = true;
+    this.id = id;
+    this.newBody = text;
+  }
+
+  updateComment(comment: Comment) {
+    comment.body = this.newBody;
+    this.commentService.updateComment(this.id, comment).subscribe(
+      () => {
+        this.getComments();
+      }
+    );
+    this.editComment = false;
+  }
+
+  back() {
+    this.editComment = false;
+    this.editPost = false;
+    this.size = '52vh';
+  }
+
+  addComment() {
+    this.size = '36vh';
+    this.showComment = true;
+    this.editComment = false;
+  }
+
+  submitComment() {
+    var comment: Comment = {
+      cId: 0,
+      body: this.commentBody,
+      created: new Date(),
+      authorId: this.user.id,
+      root: this.post
+    }
+
+    this.commentService.addComment(comment).subscribe(
+      () => {
+        this.getComments();
+      }
+    );
+    this.commentBody = null;
+    this.showComment = false;
+    this.size = '52vh';
+  }
+
+}
